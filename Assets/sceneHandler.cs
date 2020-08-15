8913ee0aa030d462a4fea86f562f0f1c3100f11c
Jaime Diaz <jaime.diaz-gonzalez-external@capgemini.com>
2020-08-15 18:57:12 +0200

    First working version

---
+++ Assets/sceneHandler.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneHandler : MonoBehaviour
+{
+    void Start()
+    {
+        // Only specifying the sceneName or sceneBuildIndex will load the Scene with the Single mode
+        Debug.Log("Bye: ");
+        SceneManager.LoadScene(1, LoadSceneMode.Additive);
+        Debug.Log("Hello: ");
+    }
+}
