bded28f885bb67f8d5886c9b72aa84d54b20b5ed
Jaime Diaz <jaime.diaz-gonzalez-external@capgemini.com>
2020-09-08 23:14:10 +0200

    Animations stopped

--- Assets/ZombieManager.cs
+++ Assets/ZombieManager.cs
@@ -5,27 +5,33 @@ using UnityEngine;
 public class ZombieManager : MonoBehaviour
 {
 
-    public float health = 50;
+    public float health = 150;
+
+    public Animator anim;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
+        anim.SetBool("shouldIdle", true);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     void ChangeHealth(float damage)
     {
-        Debug.Log("Me has dado con un danyo de: " + damage);
-        health = health - damage;
+        // Damage has a negative value
+        health += damage;
+        Debug.Log("Damage: " + damage);
+        Debug.Log("health: " + health);
         if (health < 0) 
         {
             Debug.Log("Vida por debajo de cero");
+            anim.Play("deaath");
         }
     }
 }
