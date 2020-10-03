7aa53c9d109396882f614ba7389e186a9b9951f7
Jaime Diaz <jaime.diaz-gonzalez-external@capgemini.com>
2020-10-03 16:32:31 +0200

    Sounds changed, rocket launcher added

---
+++ Assets/ShellCollisionAudioHandler.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShellCollisionAudioHandler : MonoBehaviour
+{
+    AudioSource shellSound;
+    bool notYetPlayed = true;
+    public string layerName = "Default";
+
+    void Start()
+    {
+        shellSound = GetComponent<AudioSource>();
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (notYetPlayed && isCollisionWithLayer(collision)) 
+        {
+            shellSound.Play();
+            notYetPlayed = false;
+        }
+    }
+
+    bool isCollisionWithLayer(Collision collision) {
+        if (collision.gameObject.layer == LayerMask.NameToLayer(layerName))
+        {
+            return true;
+        }
+        return false;
+    }
+}
